364763035936cd14234755858484226cd670d129
Patrick Ferreira Carvalho <patrickferreiraC911@gmail.com>
2025-04-22 22:58:07 -0300

    :sparkles: Create Cadastre-se Page form

--- pages/CadastreSePage.jsx
+++ pages/CadastreSePage.jsx
@@ -1,10 +1,33 @@
-import { View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import { Logo } from '../components/Logo'
+import { CadastreSeForm } from '../components/CadastreSeForm'
+import { Sugestao } from '../components/Sugestao'
 
 export const CadastreSePage = () => {
   return (
     <View>
       <Logo />
+      <View style={styles.form}>
+        <CadastreSeForm />
+      </View>
+      <View style={styles.sugestao}>
+        <Sugestao
+          message="Já está cadastrado?"
+          goto="Login"
+          onNavigate={() => {}}
+        />
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  form: {
+    marginTop: 38,
+    marginBottom: 12,
+  },
+  sugestao: {
+    alignItems: 'flex-end',
+    width: '92%',
+  },
+})
