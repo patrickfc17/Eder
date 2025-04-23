a4bd9e1aaf00feb3c025e4bd48f56dfe4af415f0
Patrick Ferreira Carvalho <patrickferreiraC911@gmail.com>
2025-04-22 21:00:00 -0300

    :lipstick: Create Input Component

---
+++ components/Input.jsx
@@ -0,0 +1,46 @@
+import { FontAwesome } from '@expo/vector-icons'
+import { StyleSheet, Text, TextInput, View } from 'react-native'
+import config from '../styles.config'
+
+const { dark, lighter, light } = config.colors
+
+export const Input = ({ label, icon, placeholder = '' }) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.label}>
+        <FontAwesome name={icon} size={20} color={dark} />
+        <Text style={styles.text}>{label}</Text>
+      </View>
+      <TextInput
+        style={styles.input}
+        placeholder={placeholder}
+        placeholderTextColor={light}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    gap: 12,
+    height: 79,
+  },
+  label: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 16,
+  },
+  text: {
+    fontSize: 20,
+    color: dark,
+  },
+  input: {
+    flex: 2,
+    width: 329,
+    height: 44,
+    backgroundColor: dark,
+    borderRadius: 6,
+    paddingInline: 12,
+    color: lighter,
+  },
+})
